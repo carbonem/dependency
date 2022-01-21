d9632e78b3448a887025c5fe9775522646a3a015
Dawit Tirore <dawit.tirore@gmail.com>
2022-01-21 18:12:31 +0100

    clean up

--- theories/Graph.v
+++ theories/Graph.v
@@ -2,159 +2,16 @@ Require Import Dep.Syntax.
 
 Require Import Lists.List.
 From mathcomp Require Import all_ssreflect.
+From mathcomp Require Import zify.
+From mathcomp Require Import finmap.
 From Equations Require Import Equations.
 Set Implicit Arguments.
 Unset Strict Implicit.
 Unset Printing Implicit Defensive.
 Require Import Program. 
-Require Import Lia.
 
-Ltac uf name := rewrite /name -/name.
-
-Section Enumeration.
-Variable U : countType.
-Implicit Type g : gType U.
-Fixpoint gType_size g :=
-match g with
- | GMsg a u g0 => 1 + (gType_size  g0)
- | GRec g0 => 1 + (gType_size g0)
- | GBranch a gs => 1 + (sumn (map gType_size gs))
- | GEnd => 1
-(* | GPar g0 g1 => 1 + (gType_size g0) + (gType_size g1)*)
- | GVar n => 1
-end.
-
-Fixpoint mu_height g :=
-match g with
-| GRec g0 => (mu_height g0).+1
-| _ => 0
-end.
-
-Fixpoint unf_recs_aux n g  :=
-if n is n'.+1 then
-  if g is GRec g' then unf_recs_aux n' (g'[g])
-  else g
-else g.
-
-Notation unf_recs g := (unf_recs_aux (mu_height g) g).
-
-
-Lemma contractive_mu_height : forall (g0 g1 : gType U) i, contractive_i (S i) g0 -> mu_height (substitution i g0 g1) = mu_height g0.
-Proof. 
-elim; try solve [by rewrite /=].
-- rewrite /=. intros. case : (eqVneq n i) H. move=>->. by rewrite ltnn. by rewrite /=. 
-- intros. rewrite /=. f_equal. apply H. done. 
-Qed.
-
-
-Lemma bound_lt : forall (g : gType U) i j, i < j -> bound_i i g -> bound_i j g.
-Proof. 
-elim/gType_ind_list.
-- rewrite /=;auto. move=>n i j.  move=> /leP H /ltP H1. apply/ltP.  lia. 
-- rewrite /=;auto. intros. apply : (@H i.+1 j.+1); done. 
-- rewrite /=;auto. 
-- intros. move : H1. rewrite /=.  move/allP=>H2. apply/allP. move=> l' Hin. move : (H2 l' Hin). 
-  apply H;auto. 
-- intros. simpl in H. done.
-- rewrite /=. intros. case : H1 H3. rewrite inE. case/orP. move/eqP=>->. eauto. 
-  eauto.
-Qed.
-
-
-Lemma bound_0 : forall (g : gType U) j, bound g -> bound_i j g.
-Proof.
-intros. case : j. done. intros. apply  : (@bound_lt _ 0). done. done.
-Qed.
-
-Lemma bound_subst : forall (g g': gType U) i, bound_i (S i) g -> bound g' -> bound_i i (substitution i g g').
-Proof.
-elim/gType_ind_list.
--  rewrite /=;intros;case : (eqVneq n i).
-   move=><-. apply bound_0. done. rewrite /bound_i. move : H. move=> /ltP H1 /eqP H2. 
-   apply/ltP. lia. 
-- rewrite /=. done. 
-- rewrite /=. intros. apply H. done. done.
-- rewrite /=. intros. auto. 
-- rewrite /=. intros. move : (allP H0)=> H2. apply/allP. move => l' /mapP. case. intros. 
-  subst. apply H;auto.
-- rewrite /=. move => g. by rewrite in_nil. 
-- move => g H l IH g0. rewrite inE. case/orP. move/eqP =>->; auto. auto. 
-Qed.
-
-Lemma contractive_lt : forall (g : gType U) i j, j < i -> contractive_i i g -> contractive_i j g.
-Proof.
-elim;auto.
-- rewrite /=. move => n i j. move/ltP=> + /leP;intros.  apply/leP. lia. 
-- move => g H.  rewrite /=. intros. have : j.+1 < i.+1. apply/ltP. move : H0. move/ltP. lia. eauto. 
-Qed.
-
-Lemma contractive_0 : forall (g : gType U) i,  contractive_i i g -> contractive g.
-Proof.
-move=> g. case. done. intros.  apply : contractive_lt; last eauto. done.
-Qed.
-
-Lemma bound_contractive_subst : forall (g g': gType U) i j, bound_i (S i) g -> contractive_i j g -> bound g' -> (forall j, contractive_i j g') -> 
-contractive_i j (substitution i g g').
-Proof.
-elim/gType_ind_list; (try solve [rewrite /= ;auto]).
-- rewrite/=. move => v g' i j. case : (eqVneq v i).  done. simpl. done.
-- rewrite /=. intros. apply/allP. move=> gsub /mapP. case. intros. subst. 
-  apply H;auto. auto using (allP H0), (allP H1). auto using (allP H1). 
-- rewrite /=. intros.  move : H1 H2 H3. rewrite inE. case/orP. move/eqP=>->. 
-  eauto. eauto. 
-Qed.
-
-Lemma bound_cont_eq : forall (g : gType U) i, bound_i i g -> contractive_i i g -> (forall j, contractive_i j g).
-Proof.
-elim; rewrite/=;auto.
-- rewrite /=. move => v i /ltP + /leP. lia. 
-- rewrite /=. intros. eauto. 
-Qed.
-
-Lemma unf_recs_helper : forall n g, bound g -> contractive g -> mu_height (unf_recs_aux n g) = (mu_height g) - n.
-Proof.
-elim. 
-- intros. rewrite /=. Search _ (_ - _). elim : (mu_height g);done. 
-- intros. rewrite /=. case : g H0 H1; rewrite /= //=.
-intros. rewrite subSS. rewrite H. rewrite contractive_mu_height //=.
-- auto using bound_subst. 
-- apply bound_contractive_subst;auto. apply : contractive_0. eauto.  apply : bound_cont_eq. 
- eauto. rewrite /=. done. 
-Qed.
-
-(*unf_recs unfolds all mu's, if g is bound and contractive, this is only relevant
- for the specification of linearity
- We trust that act_of_g and g_next do as intended*)
-Lemma unf_recs_fully : forall g, bound g -> contractive g -> mu_height (unf_recs g) = 0. 
-Proof. 
-intros. rewrite unf_recs_helper //=. rewrite subnE /=.
-remember (mu_height g). clear Heqn. elim : n. done. done. 
-Qed.
-
-Definition act_of_g g :=
-match unf_recs g with 
-| GMsg a _ _ => Some a
-| GBranch a _ => Some a
-| _ => None
-end.
-
-Definition g_next g :=
-match unf_recs g with 
-| GMsg _ _ g0 => [::g0]
-| GBranch _ gs => gs
-| _ => [::]
-end.
-
-Fixpoint gType_enum_aux n gs g :=
-if n is n'.+1 then
-  if g \in gs then gs
-   else foldl (gType_enum_aux n') (g::gs) (g_next g)
-else gs.
-
-Definition gType_enum (g : gType U) := undup (filter (fun g_f : gType U => act_of_g g_f != None) (gType_enum_aux (gType_size g) nil g)).
-End Enumeration.
-Arguments g_next {_}.
-Arguments gType_enum {_}.
+(*Much in this file is not necessary. We need an enumeration, its cartesian product, a filter of the action prefixed values and then the in_dep and out_dep relations
+  fingraph is not needed, so scratch the rest*)
 
 Section Node.
 Variable U : countType.
@@ -162,8 +19,8 @@ Variable g : gType U.
 
 Inductive node := Node (g_n : gType U) of g_n \in gType_enum g. 
 
-Definition node_project (n : node) : gType U := let: Node p _ := n in p. 
-Canonical node_subType := [subType for node_project]. 
+Definition g_of_n (n : node) : gType U := let: Node p _ := n in p. 
+Canonical node_subType := [subType for g_of_n]. 
 Definition node_eqMixin := Eval hnf in [eqMixin of node by <:].
 Canonical node_eqType := Eval hnf in EqType node node_eqMixin.
 Definition node_choiceMixin := [choiceMixin of node by <:].
@@ -185,21 +42,11 @@ Proof. rewrite pmap_sub_uniq //=.
 rewrite /gType_enum. apply undup_uniq. 
 Qed.
 
-Lemma in_undup : forall (A : eqType) (l : seq A) (a : A), a \in (undup l) -> a \in l.
-Proof. 
-move => A. elim. 
-- by rewrite /=.
-- move => a l IH a0.  rewrite /=. case: (@idP (a \in l)). 
- + intros. rewrite /=. rewrite inE. case : (eqVneq a0 a). done. 
-   rewrite /=. auto. 
- + move => H. rewrite inE. case/orP. move/eqP=>->. by rewrite inE eqxx.
-  move/IH. rewrite inE. move=>->. apply/orP;auto. 
-Qed.
 
-Lemma action_exists : forall n, { a : action | act_of_g (node_project n) = Some a }.
+Lemma action_exists : forall n, { a : action | act_of_g (g_of_n n) = Some a }.
 Proof. 
-elim. rewrite /gType_enum. move => gn i. move : (in_undup i). rewrite mem_filter. move/andP. case. 
-intros. destruct (act_of_g gn) eqn:Heqn. eauto.  done. 
+elim. rewrite /gType_enum. move => gn i. destruct (act_of_g gn) eqn:Heqn. exists a. 
+rewrite /=. rewrite Heqn. done. exfalso. move : i. by rewrite mem_undup mem_filter Heqn /=.
 Qed.
 
 Definition act_of_n (n : node) := let: exist a _ := action_exists n in a. 
@@ -210,12 +57,15 @@ Canonical node_subFinType := Eval hnf in [subFinType of node].
 End Node.
 Arguments Node {_}.
 Arguments gType_enum {_}.
+Arguments g_of_n {_} {_}. 
 Coercion act_of_n : node >-> action. 
-Coercion node_project : node >-> gType.
+Coercion g_of_n : node >-> gType.
 
-Section Order.
+
+Section Linearity.
 Variable (U : countType). 
 Variable (g : gType U).
+Hypotheses (Hbound : bound g) (Hcont : contractive g).
 
 Definition action_from (a : action) := let : Action p0 _ _ := a in p0.
 Definition action_to (a : action) := let : Action _ p1 _ := a in p1.
@@ -229,21 +79,58 @@ if act_of_g g0 is Some a0
   else false 
 else false.
 
+Lemma act_of_g_of_n : forall (n : node g), act_of_g (g_of_n n) = Some (act_of_n n).
+Proof.
+case. intros. rewrite /=. rewrite /act_of_n. 
+case : (action_exists (Node g g_n i)). rewrite /=. done.  
+Qed.
+
+
+Lemma opt_relP (n0 n1 : node g) (P : rel action) : reflect (P n0 n1) (opt_rel n0 n1 P).
+Proof. rewrite /opt_rel.  rewrite !act_of_g_of_n. apply idP. 
+Qed.
+
+
 Definition II_aux (a0 a1 : action) := (action_to a0 == action_to a1).
 Definition II (n0 n1 : node g) := (grel g_next n0 n1) && II_aux n0 n1.
 Definition II_spec (g0 g1 : gType U) := (grel g_next g0 g1) && opt_rel g0 g1 II_aux. 
 
+Lemma IIP (n0 n1 : node g) : reflect (II_spec n0 n1) (II n0 n1).
+Proof. 
+rewrite /II /II_spec. apply Bool.iff_reflect. split;
+ move/andP=>[H0 H1]; apply/andP; split;auto; apply/opt_relP; done.
+Qed.
+
 Definition IO_aux (a0 a1 : action) := (action_to a0 == action_from a1).
 Definition IO (n0 n1 : node g) := (grel g_next n0 n1) && IO_aux n0 n1.
 Definition IO_spec (g0 g1 : gType U) := (grel g_next g0 g1) && opt_rel g0 g1 IO_aux. 
+Lemma IOP (n0 n1 : node g) : reflect (IO_spec n0 n1) (IO n0 n1).
+Proof. 
+rewrite /IO /IO_spec. apply Bool.iff_reflect. split;
+ move/andP=>[H0 H1]; apply/andP; split;auto; apply/opt_relP; done.
+Qed.
 
 Definition OO_aux (a0 a1 : action) := (action_from a0 == action_from a1) && (action_ch a0 == action_ch a1).
 Definition OO (n0 n1 : node g) := (grel g_next n0 n1) && OO_aux n0 n1.
 Definition OO_spec (g0 g1 : gType U) := (grel g_next g0 g1) && opt_rel g0 g1 OO_aux. 
+Lemma OOP (n0 n1 : node g) : reflect (OO_spec n0 n1) (OO n0 n1).
+Proof. 
+rewrite /OO /OO_spec. apply Bool.iff_reflect. split;
+ move/andP=>[H0 H1]; apply/andP; split;auto; apply/opt_relP; done.
+Qed.
 
 Definition in_dep (n0 n1 : node g) := [exists n', connect IO n0 n' && II n' n1] || II n0 n1. 
 Definition in_dep_spec (g0 g1 : gType U) := (exists (g' : gType U), (exists2 p, path IO_spec g0 p & g' = last g0 p) /\ II_spec g' g1) 
                                       \/ II_spec g0 g1.
+
+Definition in_dep_spec_aux (n0 n1 : node g) := (exists (n' : node g), (exists2 p : seq (node g), path IO_spec n0 (map g_of_n p) & n' = last n0 p) /\ II_spec n' n1) \/ II_spec n0 n1.
+
+Lemma in_dep_in_dep_spec_aux : forall (n0 n1 : node g), in_dep n0 n1 <-> in_dep_spec_aux n0 n1.
+Proof. Admitted.
+
+
+Lemma in_depP (n0 n1 : node g) : reflect (in_dep_spec n0 n1) (in_dep n0 n1).
+Proof. Admitted.
 
 
 Definition OO_IO (n n' : node g) := OO n n' || IO n n'.
@@ -251,13 +138,22 @@ Definition OO_IO_spec (g g' : gType U) := OO_spec g g' || IO_spec g g'.
 Definition out_dep (n0 n1 : node g) := connect OO_IO n0 n1. 
 Definition out_dep_spec(g0 g1 : gType U) := exists2 p, path OO_IO_spec g0 p & g1 = last g0 p.
 
-Definition adjacent_rel (n0 n1 : node g) := (grel g_next) n0 n1.
-Definition linear := [forall n0 : node g, forall (n1 : node g | connect adjacent_rel n0 n1 && same_ch n0 n1), in_dep n0 n1 && out_dep n0 n1].
+(*Definition adjacent_rel (n0 n1 : node g) := (grel g_next) n0 n1.*)
+Definition linear := [forall n0 : node g, forall (n1 : node g | connect (grel g_next) n0 n1 && same_ch n0 n1), in_dep n0 n1 && out_dep n0 n1].
+
 Definition linear_spec := 
 forall (g0 g1 : gType U), (exists2 p, path (grel g_next) g p & g0 = last g p) ->
+                     (exists2 p, path (grel g_next) g p & g1 = last g p) ->
                      (exists2 p, path (grel g_next) g0 p & g1 = last g0 p) ->
-  opt_rel g0 g1 same_ch -> bound g -> contractive g -> in_dep_spec g0 g1 /\ out_dep_spec g0 g1.
+                     opt_rel g0 g1 same_ch -> in_dep_spec g0 g1 /\ out_dep_spec g0 g1.
 
 Lemma linearP : reflect linear_spec linear.
-Proof. Admitted.
-End Order. 
+Proof.
+Admitted.
+End Linearity. 
+
+(*define what a race is later*)
+
+(*sanity checks*)
+
+
--- theories/Syntax.v
+++ theories/Syntax.v
@@ -56,8 +56,7 @@ Inductive gType  :=
   | GEnd : gType
   | GRec : gType -> gType
   | GMsg : action -> U -> gType-> gType
-  | GBranch : action  -> seq gType -> gType
-  | GPar : gType -> gType -> gType.
+  | GBranch : action  -> seq gType -> gType.
 Set Elimination Schemes. 
 
 Section Elimination.
@@ -70,7 +69,6 @@ Hypothesis Pg_end : Pg GEnd.
 Hypothesis Pg_rec : (forall g : gType, Pg g -> Pg (GRec g)).
 Hypothesis Pg_msg : (forall (a : action) (u : U) (g : gType), Pg g -> Pg (GMsg a u g)).
 Hypothesis Pg_branch : (forall (a : action) (l : seq gType), P_glist l  -> Pg (GBranch a l)).
-Hypothesis Pg_par : (forall g : gType, Pg g -> forall g0 : gType, Pg g0 -> Pg (GPar g g0)).
 
 Hypothesis P_glist_0 : P_glist nil.
 Hypothesis P_glist_cons : forall g, Pg g -> forall l, P_glist l -> P_glist (g::l).
@@ -88,7 +86,6 @@ fix gType_rect g :=
    | GRec g => Pg_rec (gType_rect g)
    | GMsg a u g => Pg_msg a u (gType_rect g)
    | GBranch a l => Pg_branch a (seq_gType_rect l)
-   | GPar g0 g1 => Pg_par (gType_rect g0) (gType_rect g1)
    end.
 
 Definition seq_gType_rect : forall gs, P_glist gs :=
@@ -110,109 +107,38 @@ Definition gType_choiceMixin := [derive choiceMixin for gType].
 Canonical gType_choiceType := ChoiceType gType gType_choiceMixin.
 Definition gType_countMixin := [derive countMixin for gType].
 Canonical gType_countType := CountType gType gType_countMixin.
-
 Check mut_rect.
-Definition gType_ind P := @mut_rect P (fun gs => forall g, List.In g gs -> P g).
-(*Lemma gType_ind :
-  forall (P : gType -> Prop),
-    P GEnd ->
-    (forall v, P (var_gType v)) ->
-    (forall G, P G -> P (GRec G)) ->
-    (forall a gs, (forall g, List.In g gs -> P g) -> P (GBranch a gs)) ->
-    (forall a u g0, P g0 -> P (GMsg a u g0)) ->
-    (forall g0 g1, P g0 -> P g1 -> P (GPar g0 g1)) ->
-    forall g : gType, P g.
-Proof.
-  move=> P P_end P_var P_rec P_branch P_msg P_par. fix Ih 1; case.
-  + by apply: P_var.
-  + by apply: P_end.
-  + by move=>G; apply: P_rec.
-  + move=> a s g; apply: P_msg. done. 
-  + move => a l. apply P_branch. intros. elim : l H. 
-   * simpl. contradiction. 
-   * move=> a0 l H. simpl. case. move =><-. apply Ih. done. 
-  + move => g g0. by apply : P_par. 
-Qed.*)
 
-(*
-Definition gType_der := [indDef for gType_ind].
+Definition gType_ind P := @gType_rect P (fun gs => forall (g : gType), true ).
 
-Fixpoint gType_eqb g0 g1 := 
-let list_eq := fix list_eq s1 s2 {struct s1} := 
-                                 match s1,s2 with
-                                  | [::] , [::] => true 
-                                  | x1 :: s1', x2 :: s2' =>  gType_eqb x1 x2 && (list_eq s1' s2') 
-                                  | _ , _ => false 
-                                  end
-in
-match g0, g1 with
-| GMsg a0 v0 g0', GMsg a0' v0' g1' => (a0 == a0') && (v0 == v0') && (gType_eqb g0' g1') 
-| GRec g0', GRec g1' => gType_eqb g0' g1' 
-| GEnd, GEnd => true
-| GPar g0' g0'', GPar g1' g1'' => (gType_eqb g0' g1') && (gType_eqb g0'' g1'')
-| GBranch a gs, GBranch a' gs' => (a == a') && (list_eq gs gs')
-| var_gType n, var_gType n' => n == n'
-| _, _ => false
-end.
-
-Ltac case_filter :=  case;try (rewrite /= ; constructor ; done);intros.
-Ltac case_filter_on x := generalize dependent x;case_filter.
-Ltac inj := split; solve [ by move/eqP=>-> | by case; move=>->].
-
-Ltac inject :=  split; [ by move=>-> | by case ].
-Ltac inject2 :=  split;[ case; by move =>->-> | case;by move =>->-> ]. 
-Ltac inject3 := split; [ case;  move=>->; case; move=>->->; done |  case; move =>->->->; done]. 
-
-
-
-Lemma reflexive_gType :  reflexive gType_eqb.
-Proof. unfold reflexive.
-elim;try solve [ rewrite /= ; done].
-- intros. rewrite /=. induction gs; first by rewrite eqxx.
-  rewrite eqxx /=. apply/andP. split. apply H. simpl. auto. 
-  have : (forall g, List.In g gs -> gType_eqb g g). intros. apply H. simpl; auto. 
-  move/IHgs. move/andP => [] _. done. 
-- intros. by rewrite /= !eqxx H. 
-- intros. by rewrite /= H H0.
+(*Remove vacously true sub goal from principle*)
+Lemma gType_ind_list P : 
+       (forall n : nat, P (GVar n)) ->
+       P GEnd ->
+       (forall g : gType, P g -> P (GRec g)) ->
+       (forall (a : action) (u : U) (g : gType), P g -> P (GMsg a u g)) ->
+       (forall (a : action) (l : seq gType),
+        (forall g : gType_eqType, g \in l -> P g) -> P (GBranch a l)) ->
+       (forall g : gType,
+        P g ->
+        forall l : seq gType,
+        (forall g0 : gType_eqType, g0 \in l -> P g0) ->
+        forall g0 : gType_eqType, g0 \in g :: l -> P g0) -> 
+       forall g : gType, P g.
+Proof. 
+intros. move : g. apply : (@gType_rect P (fun gs => forall g, g \in gs -> P g));auto.
+move => g. rewrite in_nil. done.
 Qed.
 
 
-Lemma gType_eqb_axiom : Equality.axiom gType_eqb.
-Proof.
-unfold Equality.axiom. intros.  apply Bool.iff_reflect.
-generalize dependent y. generalize dependent x.
-elim. 
-- case_filter.
-- move=>n. case_filter. rewrite /=. split. case; first by (move=>->; rewrite eqxx). 
-  move/eqP=>->. done.
-- move=> g Hg. case_filter. split. 
- * case; first (move=>->; rewrite /=). by rewrite reflexive_gType. 
- * rewrite /=. rewrite <- Hg. move=>->. done.
-- move => a gs H. case_filter. split.
- * case. move=>->->. by rewrite reflexive_gType. 
- * intros. f_equal. simpl in H0. by move : (andP H0) =>[]=>/eqP. simpl in H0. 
-   move : (andP H0)=>[] _. clear H0. generalize dependent l.  induction gs;case_filter.  done. 
-   done. 
-   move : (andP b). case. move/H=>H1 H2. rewrite H1. f_equal. apply IHgs. 
-   intros. apply H. simpl. auto. done. simpl;auto. 
--
- move=> a u g0 H. case_filter. split. case. move=>->->->. rewrite reflexive_gType. done.
- rewrite /=. move/andP=>[]/andP[]/eqP +/eqP=>->->. move/H=>->. done. 
-- move=> g0 g1 H0 H1. case_filter. rewrite /=. split. case. move=>->->.
-  rewrite !reflexive_gType. done.
-  move/andP=>[] /H0 + /H1=>->->.  done.
-Qed.
- 
-
-Definition gType_eqMixin := EqMixin gType_eqb_axiom. 
-Canonical gType_EqType := EqType gType gType_eqMixin.*)
+Definition gType_rec_list P := @gType_ind_list P. 
 
 Fixpoint bound_i (i : nat) (g : gType) := 
 match g with 
 | GEnd => true
 | GMsg _ _ g0 => bound_i i g0
 | GBranch _ gs => all (bound_i i) gs
-| GPar g0 g1 => (bound_i i g0) && (bound_i i g1)
+(*| GPar g0 g1 => (bound_i i g0) && (bound_i i g1)*)
 | GRec g0 => bound_i (S i) g0
 | GVar n => n < i
 end.
@@ -223,16 +149,12 @@ match g with
 | GEnd => true
 | GMsg _ _ g0 => contractive_i 0 g0
 | GBranch _ gs => all (contractive_i 0) gs
-| GPar g0 g1 => (contractive_i d g0) && (contractive_i d g1)
+(*| GPar g0 g1 => (contractive_i d g0) && (contractive_i d g1)*)
 | GRec g0 => contractive_i (S d) g0
 | GVar n => d <= n
 end. 
 
-(*
-Inductive wfgType n := WFgType g of bound_i n g && contractive_i 0 g.
-Coercion gType_of_wf n (wfg : wfgType n)  := let: WFgType g _ := wfg in g.
-Canonical wfgType_subType n := [subType for (@gType_of_wf n)].
-Definition GType := wfgType 0. *)
+
 
 Fixpoint substitution (i : nat) (g0 g1 : gType) :=
 match g0 with
@@ -240,7 +162,7 @@ match g0 with
 | GBranch a gs => GBranch a (map (fun g0' => substitution i g0' g1) gs)
 | GVar n => if n == i then g1 else g0
 | GRec g0' => GRec (substitution (S i) g0' g1)
-| GPar g0' g1' => GPar (substitution i g0' g1) (substitution i g1' g1)
+(*| GPar g0' g1' => GPar (substitution i g0' g1) (substitution i g1' g1) *)
 | GEnd => GEnd
 end.
 
@@ -253,7 +175,8 @@ Arguments substitution {_}.
 Notation bound := (bound_i 0).
 Notation contractive := (contractive_i 0).
 
-Notation "G '[' G0 ']'" := (substitution 0 G G0)(at level 0).
+Notation "G [ G0 ]" := (substitution 0 G G0)(at level 0, format "G [ G0 ]").
+
 Notation gt_pred := (fun n0 n1 g => bound_i n0 g && contractive_i n1 g).
 
 
@@ -263,9 +186,14 @@ Arguments GMsg {_}.
 Arguments GBranch {_}.
 Arguments GVar {_}.
 Arguments GRec {_}.
-Arguments GPar {_}.
+(*Arguments GPar {_}.*)
 Arguments GEnd {_}.
 
+Definition linear_spec := 
+forall (g0 g1 : gType U), (exists2 p, path (grel g_next) g p & g0 = last g p) ->
+                     (exists2 p, path (grel g_next) g p & g1 = last g p) ->
+                     (exists2 p, path (grel g_next) g0 p & g1 = last g0 p) ->
+                     opt_rel g0 g1 same_ch -> in_dep_spec g0 g1 /\ out_dep_spec g0 g1.
 
 
 
